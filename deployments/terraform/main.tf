3d7c40dc49b107d849a79e71a7c2754375d7e4c0
seckinemrah <seckinemrah@gmail.com>
2020-07-25 19:41:19 -0500

    added main.tf file

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,15 @@
+module "artemis-deploy" {
+  source  = "fuchicorp/chart/helm"
+  deployment_name        = "artemis"
+  deployment_environment = "${var.environment}"
+  deployment_endpoint    = "none" 
+  deployment_path        = "artemis"
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"    
+  }
+}
+variable "environment" {
+}
+variable "deployment_image" {
+  default = "seckinemrah/artemis:master"
+}
